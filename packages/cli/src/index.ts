79c737f2688d8482079ce9dbeb2f344f740810a7
Dylan Feltus <contact@dylanfeltus.com>
2026-09-04 08:10:00 -0400

    Release 0.11.0 (#140)

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -643,7 +643,7 @@ export type ParsedCommand =
 
 type CliConfigFile = StratusConfigFile;
 
-export const CLI_VERSION = '0.10.0';
+export const CLI_VERSION = '0.11.0';
 
 const DASHBOARD_TITLE = 'Stratus Agent Dashboard';
 
